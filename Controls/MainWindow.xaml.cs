ac03e5eca4bfaa65cda0ae4a4cd9a132aac3113a
KlaasHagge <44665897+KlaasHagge@users.noreply.github.com>
2023-02-13 12:41:57 +0100

    Add project files.

---
+++ Controls/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Controls
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //Event-Handler für das Click-Event des Buttons
+        private void Btn_KlickMich_Click(object sender, RoutedEventArgs e)
+        {
+            //Übertrag des TextBox-Texts in den Label-Content
+            Lbl_Output.Content = Tbx_Input.Text;
+
+            //Prüfung, ob die Checkbox abgehakt ist
+            if (Cbx_Haken.IsChecked == true)
+                //Neuzuweisung der Titel-Eigenschaft des Windows mit dem ausgewählten Inhalt der ComboBox
+                this.Title = (Cbb_Auswahl.SelectedItem as ComboBoxItem)?.Content.ToString();
+
+            //Übertrag des Slider-Values in den Button-Content
+            Btn_KlickMich.Content = Sdr_Wert.Value.ToString();
+        }
+
+        //Event, das auf einen Tastendruck reagiert (wenn Cursor in TextBox)
+        private void Tbx_Input_KeyDown(object sender, KeyEventArgs e)
+        {
+            //Prüfung, welche Taste gedrückt wurde
+            if (e.Key == Key.F12)
+                //Ändern der TextWrappuing-Eigenschaft
+                Tbx_Input.TextWrapping = TextWrapping.NoWrap;
+        }
+
+        private void Schließen_Click(object sender, RoutedEventArgs e)
+        {
+            //Schließen des Fensters
+            this.Close();
+        }
+
+        private void Fenster_Click(object sender, RoutedEventArgs e)
+        {
+            Window wnd = new MainWindow();
+
+            wnd.Title = "Neues Fenster";
+
+            //Öffen eines neuen Fensters als gleichberechtigtes Fenster
+            wnd.Show();
+        }
+
+        private void Dialog_Click(object sender, RoutedEventArgs e)
+        {
+            Window wnd = new MainWindow();
+
+            wnd.Title = "Neues Dialog-Fenster";
+
+            //Öffnen eines neuen Fensters als Dialogfenster mit Rückgabe des DialogResults
+            if (wnd.ShowDialog() == true)
+                Lbl_Output.Content = "OKAY wurde geklickt";
+        }
+
+        private void Btn_Ok_Click(object sender, RoutedEventArgs e)
+        {
+            //Setzen des DialogResults (NUR IN DIALOGFENSTERN) und automatisches Schließen des Fensters
+            this.DialogResult = true;
+            this.Close();
+        }
+    }
+}
---
+++ WPF_VC_230213/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Grundlagen
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            //Dies verweist auf eine Methode in der (versteckten) automatisch generierten zweiten Klassen-Datei (*.g.i.cs),
+            //welche für das Rendering des XAML-Codes verantwortlich ist. InitializeComponent() erstellt die
+            //Steuerelement-Objekte und muss daher als erste Methode des Konstruktors bestehen bleiben
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //Verändern des Backgrounds der TextBox
+            Tbx_Input.Background = new SolidColorBrush(Colors.Yellow);
+
+            //Ändern einer UI-Property über den Sender-Parameter (beinhaltet den Verursacher des Events)
+            (sender as Button).Content = "Button wurde angeklickt";
+        }
+    }
+}
