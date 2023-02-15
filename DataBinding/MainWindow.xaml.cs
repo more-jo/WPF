f973113773a261837eb9c63694a797c1f6db7a9a
KlaasHagge <44665897+KlaasHagge@users.noreply.github.com>
2023-02-15 12:00:36 +0100

    DataBindings

---
+++ DataBinding/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DataBinding
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.DataContext = this;
+        }
+
+        public ObservableCollection<Person> Personenliste { get; set; } = new ObservableCollection<Person>()
+        {
+            new Person(){Name = "Anna Nass", Alter=34},
+            new Person(){Name = "Rainer Zufall", Alter=65},
+            new Person(){Name = "Maria Meier", Alter=12},
+        };
+
+        private void Btn_Show_Click(object sender, RoutedEventArgs e)
+        {
+            Person person = Spl_DataContextBsp.DataContext as Person;
+
+            MessageBox.Show(person.Name + " (" + person.Alter + ")");
+        }
+
+        private void Btn_Altern_Click(object sender, RoutedEventArgs e)
+        {
+            (Spl_DataContextBsp.DataContext as Person).Alter++;
+        }
+
+        private void Btn_NewDay_Click(object sender, RoutedEventArgs e)
+        {
+            Person person = Spl_DataContextBsp.DataContext as Person;
+            person.WichtigeTage.Add(new DateTime(2022, 12, 3));
+            person.UpdateGUI();
+        }
+
+        private void Btn_New_Click(object sender, RoutedEventArgs e)
+        {
+            Personenliste.Add(new Person() { Name = "Hugo Müller", Alter = 98 });
+        }
+
+        private void Btn_Löschen_Click(object sender, RoutedEventArgs e)
+        {
+            if (LstV_Personen.SelectedItem != null)
+                Personenliste.Remove(LstV_Personen.SelectedItem as Person);
+        }
+    }
+}
---
+++ DataBinding/Person.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBinding
+{
+    public class Person : INotifyPropertyChanged
+    {
+        private int alter;
+
+        public string Name { get; set; }
+        public int Alter 
+        { 
+            get => alter;
+            set
+            {
+                alter = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Alter)));
+            }
+        }
+
+        public List<DateTime> WichtigeTage { get; set; } = new List<DateTime>()
+        {
+            DateTime.Now,
+            new DateTime(2002, 2, 1),
+            new DateTime(2023, 5, 12)
+        };
+
+        public DateTime LastObject
+        {
+            get { return WichtigeTage.Last(); }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public void UpdateGUI()
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(LastObject)));
+        }
+
+
+        public override string ToString()
+        {
+            return this.Name + " (" + this.Alter + ")";
+        }
+    }
+}
