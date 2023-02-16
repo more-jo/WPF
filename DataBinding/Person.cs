286888071c2a31ee6df65be837f4e22bfc33b5ad
KlaasHagge <44665897+KlaasHagge@users.noreply.github.com>
2023-02-16 09:33:43 +0100

    DataBinding

--- DataBinding/Person.cs
+++ DataBinding/Person.cs
@@ -7,26 +7,28 @@ using System.Threading.Tasks;
 
 namespace DataBinding
 {
+    //Das Interface INotifyPropertyChanged sorgt für ein neues Event, welches bei Aktivierung die GUI über eine Veränderung in diesem Objekt informiert
     public class Person : INotifyPropertyChanged
     {
-        private int alter;
+        //Eine Datenbindung kann nur an Properties durchgeführt werden (keine Felder)
+        public string Vorname { get; set; }
+        public string Nachname { get; set; }
 
-        public string Name { get; set; }
-        public int Alter 
-        { 
-            get => alter;
+        private int alter;
+        public int Alter
+        {
+            get { return alter; }
             set
             {
                 alter = value;
+                //Das PropertyChanged-Event muss zu dem Zeitpunkt geworfen werden, zu dem die GUI über eine Veränderung informiert werden soll
                 PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Alter)));
             }
         }
 
         public List<DateTime> WichtigeTage { get; set; } = new List<DateTime>()
         {
-            DateTime.Now,
-            new DateTime(2002, 2, 1),
-            new DateTime(2023, 5, 12)
+            new DateTime(2003, 12, 3)
         };
 
         public DateTime LastObject
@@ -34,17 +36,19 @@ namespace DataBinding
             get { return WichtigeTage.Last(); }
         }
 
-        public event PropertyChangedEventHandler? PropertyChanged;
-
-        public void UpdateGUI()
+        //Methode zur GUI-Aktualisierung (muss aufgerufen werden, wenn die Oberfläche über eine Veränderung von 'LastObject' informiert werden soll
+        public void UpdateLastObject()
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(LastObject)));
         }
 
+        //Durch das Interface geforderte Event
+        public event PropertyChangedEventHandler? PropertyChanged;
 
+        //Unter bestimmten Umständen (z.B. in einer Liste ohne DataTemplate) definiert die ToString()-Funktion das Aussehen der Objekte in der GUI
         public override string ToString()
         {
-            return this.Name + " (" + this.Alter + ")";
+            return $"{Vorname} {Nachname} ({Alter})";
         }
     }
 }
