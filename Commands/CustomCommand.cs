48ccaea3d5712fb6aa1beab9b3783857369330f8
KlaasHagge <44665897+KlaasHagge@users.noreply.github.com>
2023-02-21 12:45:37 +0100

    Tag 7: Commands / MVVM

---
+++ Commands/CustomCommand.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Commands
+{
+    //Allgemeine generische Commandklasse, welche individuell befüllt werden kann.
+
+    //ICommand ermöglicht dieser Klasse, als Command verwendet zu werden
+    public class CustomCommand : ICommand
+    {
+        //Anmeldung des Commands im CommandManager
+        public event EventHandler? CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        //Delegates zum Speichern der Logik
+        public Action<object> ExecuteMethode { get; set; }
+        public Func<object, bool> CanExecuteMethode { get; set; }
+
+
+        //Bedingung für die Ausführung
+        public bool CanExecute(object? parameter) => CanExecuteMethode(parameter);
+
+        //Aktion bei Ausführung
+        public void Execute(object? parameter) => ExecuteMethode(parameter);
+
+
+        //Konstruktor
+        public CustomCommand(Action<object>exe, Func<object, bool>can)
+        {
+            ExecuteMethode= exe;
+            CanExecuteMethode= can;
+        }
+    }
+}
---
+++ MVVM/CustomCommand.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MVVM
+{
+    //vgl. M13_Commands
+    public class CustomCommand : ICommand
+    {
+        public event EventHandler? CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public Action<object> ExecuteMethode { get; set; }
+        public Func<object, bool> CanExecuteMethode { get; set; }
+
+        public bool CanExecute(object? parameter) => CanExecuteMethode(parameter);
+
+        public void Execute(object? parameter) => ExecuteMethode(parameter);
+
+
+        public CustomCommand(Action<object>exe, Func<object, bool>can = null)
+        {
+            ExecuteMethode= exe;
+
+            if (can == null) CanExecuteMethode = p => true;
+            else CanExecuteMethode = can;
+        }
+    }
+}
